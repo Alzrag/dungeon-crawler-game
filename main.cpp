337e4999c2b8e9bc67154ca909e725e16b8da175
Alzrag <jediclark14@icloud.com>
2026-03-25 16:08:19 -0600

    gpu detection take 2 and more

--- main.cpp
+++ main.cpp
@@ -11,7 +11,8 @@
 #include <vector>
 #include <cstring>
 #include <map>
-
+#include <optional>
+#include <set>
 const uint32_t WIDTH = 800;
 const uint32_t HEIGHT = 600;//really dont change window size its hard
 
@@ -38,9 +39,46 @@ private:
     GLFWwindow* window;
     VkInstance instance;
     VkDebugUtilsMessengerEXT debugMessenger;
- 
+    VkDevice device;
+    VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
+    VkQueue graphicsQueue;
+    VkSurfaceKHR surface;
+    VkQueue presentQueue;
+  
+    struct QueueFamilyIndicies {
+      std::optional<uint32_t> graphicsFamily;
+      std::optional<uint32_t> presentFamily;
+      bool isComplete(){
+        return graphicsFamily.has_value() && presentFamily.has_value();
+      }
+    };
+    //queue families dertminte the order of commands sent
+    QueueFamilyIndicies findQueueFamilies(VkPhysicalDevice device){
+      QueueFamilyIndicies indices;
+      uint32_t queueFamilyCount = 0;
+      vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, nullptr);//get a list of queue families
+      std::vector<VkQueueFamilyProperties> queueFamilies(queueFamilyCount);
+      vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, queueFamilies.data());//populate the list of quefamilties with data;
+      int i=0;
+      for (const auto& queueFamily :queueFamilies){
+        if (queueFamily.queueFlags & VK_QUEUE_GRAPHICS_BIT){
+          VkBool32 presentSupport = false;
+          vkGetPhysicalDeviceSurfaceSupportKHR(device, i, surface, &presentSupport);
+          if (presentSupport){
+            indices.presentFamily = i;
+          }
+          indices.graphicsFamily = i;
+        }
+        if(indices.isComplete()){
+          break;
+        }
+        i++;
+      }
+      return indices  ;
+    }
+
     void pickPhysicalDevice(){
-      VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;//hold current phsycial device
+      physicalDevice = VK_NULL_HANDLE;//hold current phsycial device
       uint32_t deviceCount =0;//numbers of availbile vulkan gpu's
       vkEnumeratePhysicalDevices(instance, &deviceCount, nullptr);//check how many there are
       if (deviceCount == 0){
@@ -48,32 +86,113 @@ private:
       }
       std::vector<VkPhysicalDevice> devices(deviceCount);//now that we know there s no error we can add them to a list
       vkEnumeratePhysicalDevices(instance, &deviceCount, devices.data());
-      for (const auto& device: devices){//first check for dedicated gpu
-        if (isDeviceSuitable(device, true)){
-          physicalDevice = device;
-          break;
-        }
+      std::multimap<int, VkPhysicalDevice> candidates;//sort them with a multimap ordred by their score
+      for  (const auto& device : devices){
+        int score = rateDeviceSuitability(device);
+        candidates.insert(std::make_pair(score, device));
       }
-      if (physicalDevice == VK_NULL_HANDLE){
-        for (const auto& device: devices){//if not dediated check intigrated
-          if (isDeviceSuitable(device, false)){
-            physicalDevice = device;
-            break;
-          }
-        }
-      }
-      if (physicalDevice == VK_NULL_HANDLE){//if neither there is a issue
-        throw std::runtime_error("failed to find a suitable GPU! (please check you device, drivers, and vulkan instialation[should have been automatic])");
+      if (candidates.rbegin()->first>0){//if the best cant support vulkan non can
+        physicalDevice = candidates.rbegin()->second;
+      } else {
+        throw std::runtime_error("failed to find GPU's with Vulkan support! (please check you device, drivers, and vulkan instialation[should have been automatic])");
       }
     }
 
-    bool isDeviceSuitable(VkPhysicalDevice device, bool dedTintF){
+    int rateDeviceSuitability(VkPhysicalDevice device){
       //start be getting the properties and features of the device
       VkPhysicalDeviceProperties deviceProperties;
       vkGetPhysicalDeviceProperties(device, &deviceProperties);
       VkPhysicalDeviceFeatures deviceFeatures;
       vkGetPhysicalDeviceFeatures(device, &deviceFeatures);
-      return deviceProperties.deviceType == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU && deviceFeatures.geometryShader && dedTintF == true || deviceProperties.deviceType == VK_PHYSICAL_DEVICE_TYPE_INTEGRATED_GPU && deviceFeatures.geometryShader && dedTintF == false;
+      VkPhysicalDeviceMemoryProperties memoryProperties;
+      vkGetPhysicalDeviceMemoryProperties(device, &memoryProperties);
+
+      int score = 0;
+      // if it cant do gemotiry shading it cant do what we need
+      if (!deviceFeatures.geometryShader) {
+        return 0;
+      }
+      //check type
+      switch (deviceProperties.deviceType){
+        case VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU:
+          score += 475;
+          break;
+        case VK_PHYSICAL_DEVICE_TYPE_VIRTUAL_GPU:
+          score += 350;
+          break;
+        case VK_PHYSICAL_DEVICE_TYPE_INTEGRATED_GPU:
+          score += 250;
+          break;
+        default:
+          score +=0;
+          break;
+      }
+      //raw stat based enchacement
+      score += deviceProperties.limits.maxImageDimension2D / 128;
+      score += deviceProperties.limits.maxImageDimension3D / 32;
+      score += deviceProperties.limits.maxImageDimensionCube / 128;
+      score += deviceProperties.limits.maxImageArrayLayers;
+      score += deviceProperties.limits.maxUniformBufferRange / 100000;
+      score += deviceProperties.limits.maxStorageBufferRange / 100000;
+      score += deviceProperties.limits.maxPushConstantsSize;
+      score += deviceProperties.limits.maxComputeSharedMemorySize / 750;
+      score += deviceProperties.limits.maxComputeWorkGroupInvocations / 2;
+      score += deviceProperties.limits.maxDrawIndirectCount > 1? 200 : 0;
+      score += deviceProperties.limits.maxDrawIndexedIndexValue > 0xFFFF? 100 : 0;
+      score += deviceProperties.limits.maxBoundDescriptorSets * 10;
+      score += (deviceProperties.limits.maxDescriptorSetSamplers > 4096 ? 4096u : deviceProperties.limits.maxDescriptorSetSamplers) / 10;//why the fuck do these get so big on modern cards
+      score += (deviceProperties.limits.maxDescriptorSetUniformBuffers > 256 ? 256u : deviceProperties.limits.maxDescriptorSetUniformBuffers) * 5;
+      score += (deviceProperties.limits.maxDescriptorSetStorageBuffers > 256 ? 256u : deviceProperties.limits.maxDescriptorSetStorageBuffers) * 5;      
+      score += deviceProperties.limits.maxVertexInputAttributes * 5;
+      score += deviceProperties.limits.maxVertexInputBindings * 5;
+      score += deviceProperties.limits.maxFramebufferWidth / 512;
+      score += deviceProperties.limits.maxFramebufferHeight / 512;
+      score += deviceProperties.limits.maxColorAttachments * 10;
+      score += deviceProperties.limits.maxSampleMaskWords * 10;
+      score += (int)deviceProperties.limits.maxSamplerAnisotropy * 10;
+      score += (int)deviceProperties.limits.maxSamplerLodBias * 5;
+      //raw feature based additives
+      if (deviceFeatures.tessellationShader) score += 100;
+      if (deviceFeatures.multiDrawIndirect) score += 100;
+      if (deviceFeatures.samplerAnisotropy) score += 50;
+      if (deviceFeatures.textureCompressionBC) score += 50;
+      if (deviceFeatures.textureCompressionETC2) score += 30;
+      if (deviceFeatures.shaderFloat64) score += 50;
+      if (deviceFeatures.shaderInt64) score += 30;
+      if (deviceFeatures.shaderInt16) score += 20;
+      if (deviceFeatures.multiViewport) score += 50;
+      if (deviceFeatures.depthClamp) score += 20;
+      if (deviceFeatures.fillModeNonSolid) score += 20;
+      if (deviceFeatures.wideLines) score += 10;
+      if (deviceFeatures.largePoints) score += 10;
+      //vram addtion calcuator biggest component
+      VkDeviceSize totalVRAM = 0;
+      for (uint32_t i = 0; i < memoryProperties.memoryHeapCount; i++) {
+          if (memoryProperties.memoryHeaps[i].flags & VK_MEMORY_HEAP_DEVICE_LOCAL_BIT) {
+              totalVRAM += memoryProperties.memoryHeaps[i].size;
+          }
+      }
+      if (deviceProperties.deviceType == VK_PHYSICAL_DEVICE_TYPE_INTEGRATED_GPU){//vulkan detects 0 from igpu but they have 128mb for intel 256mb for amd usually
+        switch(deviceProperties.vendorID){
+          case 0x8086://intel
+            totalVRAM+=128*1024*1024;
+            break;
+          case 0x1002://amd 
+            totalVRAM+=256*1024*1024;
+            break;
+          default:
+            totalVRAM+=128*1024*1024;
+            break;
+        }
+      }
+      score += (int)(totalVRAM / (1024 * 1024 * 1024)) * 200;
+
+      return score;
+    } 
+
+    bool isDeviceSuitable(VkPhysicalDevice device){
+      QueueFamilyIndicies indices = findQueueFamilies(device);
+      return indices.isComplete();
     }
 
     void createInstance() {
@@ -183,6 +302,51 @@ private:
       
       createInstance();
       setupDebugMessenger();
+      createSurface();
+      pickPhysicalDevice();
+      createLogicalDevice();
+    }
+
+    void createSurface(){
+      if (glfwCreateWindowSurface(instance, window, nullptr, &surface) != VK_SUCCESS){
+        throw std::runtime_error("Failed to create window surface");
+      }
+    }
+
+    void createLogicalDevice() { 
+      //back to extension stuff as we set up those queuefamilies from above with how manu ques they are to make
+      QueueFamilyIndicies indices = findQueueFamilies(physicalDevice);
+      std::vector<VkDeviceQueueCreateInfo> queueCreateInfos;
+      std::set<uint32_t> uniqueQueueFamilies = {indices.graphicsFamily.value(), indices.presentFamily.value()};
+      float queuePriority = 1.0f;
+      for(uint32_t queueFamily : uniqueQueueFamilies){
+        VkDeviceQueueCreateInfo queueCreateInfo{};
+        queueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+        queueCreateInfo.queueFamilyIndex = queueFamily;
+        queueCreateInfo.queueCount = 1;
+        queueCreateInfo.pQueuePriorities = &queuePriority;
+        queueCreateInfos.push_back(queueCreateInfo);
+      }   
+      VkPhysicalDeviceFeatures deviceFeatures{};
+      //now we create the logic device with the above strucutre.
+      VkDeviceCreateInfo createInfo{};
+      createInfo.sType=VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+      createInfo.pQueueCreateInfos=queueCreateInfos.data();
+      createInfo.pEnabledFeatures=&deviceFeatures;
+      createInfo.queueCreateInfoCount = static_cast<uint32_t>(queueCreateInfos.size());
+      createInfo.enabledExtensionCount = 0;//ignored
+      if (enableValidationLayers){
+        createInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
+        createInfo.ppEnabledLayerNames = validationLayers.data();
+      } else {
+        createInfo.enabledLayerCount = 0;
+      }
+      //try to create the logic device
+      if (vkCreateDevice(physicalDevice, &createInfo, nullptr, &device) != VK_SUCCESS){
+        throw std::runtime_error("failed to create logical device!");
+      }
+      vkGetDeviceQueue(device, indices.graphicsFamily.value(), 0, &graphicsQueue);
+      vkGetDeviceQueue(device, indices.presentFamily.value(), 0, &presentQueue);
     }
 
     //technically this next function exists in th4 code biut it is faster to write it ourselves then try t find and thenc all it 
@@ -226,10 +390,11 @@ private:
     }
 
     void cleanup() {
+      vkDestroyDevice(device, nullptr);
       if (enableValidationLayers){
         DestroyDebugUtilsMessengerEXT(instance, debugMessenger, nullptr);
       }
-      
+      vkDestroySurfaceKHR(instance, surface, nullptr);
       vkDestroyInstance(instance, nullptr); //i beleive that this destroyed the instance and sets its poitns to nullptr may be wrong?
       glfwDestroyWindow(window);//when window closes destroy its
       glfwTerminate();//stop the glfw library background tasks
