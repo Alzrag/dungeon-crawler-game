29e8f19ea74e0bd482e3fc12456057701c140165
Alzrag <jediclark14@icloud.com>
2026-04-08 15:38:53 -0600

    finished staging buffer

--- main.cpp
+++ main.cpp
@@ -486,34 +486,83 @@ private:
     }
 
     void createVertexBuffer(){
+      VkDeviceSize bufferSize = sizeof(verticies[0])*verticies.size();
+
+      VkBuffer stagingBuffer;
+      VkDeviceMemory stagingBufferMemory;
+      
+      createBuffer(bufferSize, VK_BUFFER_USAGE_TRANSFER_SRC_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT, stagingBuffer, stagingBufferMemory);
+      void* data;
+      vkMapMemory(device, stagingBufferMemory, 0, bufferSize,0,&data);
+      memcpy(data,verticies.data(),(size_t) bufferSize);
+      vkUnmapMemory(device, stagingBufferMemory);
+      createBuffer(bufferSize, VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_VERTEX_BUFFER_BIT, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, vertexBuffer, vertexBufferMemory);
+      
+      copyBuffer(stagingBuffer,vertexBuffer,bufferSize);
+      vkDestroyBuffer(device, stagingBuffer, nullptr);
+      vkFreeMemory(device, stagingBufferMemory, nullptr);
+    }
+
+    //in order to is maps and better acsess memory we must transfer local memeory from buffer to vertex
+    void copyBuffer(VkBuffer srcBuffer, VkBuffer dstBuffer, VkDeviceSize size){
+      VkCommandBufferAllocateInfo allocInfo{};
+      allocInfo.sType=VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
+      allocInfo.level=VK_COMMAND_BUFFER_LEVEL_PRIMARY;
+      allocInfo.commandPool=commandPool;
+      allocInfo.commandBufferCount=1;
+
+      VkCommandBuffer commandBuffer;
+      vkAllocateCommandBuffers(device, &allocInfo, &commandBuffer);
+
+      //start recording commands for the command buffer
+      VkCommandBufferBeginInfo beginInfo{};
+      beginInfo.sType=VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
+      beginInfo.flags=VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT;
+
+      vkBeginCommandBuffer(commandBuffer, &beginInfo);
+
+      VkBufferCopy copyRegion{};
+      copyRegion.srcOffset=0;
+      copyRegion.dstOffset=0;
+      copyRegion.size=size;
+      vkCmdCopyBuffer(commandBuffer, srcBuffer, dstBuffer, 1, &copyRegion);
+      
+      vkEndCommandBuffer(commandBuffer);
+
+      VkSubmitInfo submitInfo{};
+      submitInfo.sType=VK_STRUCTURE_TYPE_SUBMIT_INFO;
+      submitInfo.commandBufferCount=1;
+      submitInfo.pCommandBuffers=&commandBuffer;
+      vkQueueSubmit(graphicsQueue, 1, &submitInfo, VK_NULL_HANDLE);
+      vkQueueWaitIdle(graphicsQueue);
+
+      vkFreeCommandBuffers(device,commandPool,1,&commandBuffer);//internally this is a fence to wait until transfer finishes.
+    }
+
+    void createBuffer(VkDeviceSize size, VkBufferUsageFlags usage, VkMemoryPropertyFlags properties, VkBuffer& buffer, VkDeviceMemory& bufferMemory){
       VkBufferCreateInfo bufferInfo{};
       bufferInfo.sType=VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
-      bufferInfo.size=sizeof(verticies[0])*verticies.size();
-      bufferInfo.usage=VK_BUFFER_USAGE_VERTEX_BUFFER_BIT;
+      bufferInfo.size=size;
+      bufferInfo.usage=usage;
       bufferInfo.sharingMode=VK_SHARING_MODE_EXCLUSIVE;
 
-      if (vkCreateBuffer(device,&bufferInfo,nullptr,&vertexBuffer)!=VK_SUCCESS){
+      if (vkCreateBuffer(device,&bufferInfo,nullptr,&buffer)!=VK_SUCCESS){
         throw std::runtime_error("failed to create vertex buffer");
       }
 
       VkMemoryRequirements memRequirements;
-      vkGetBufferMemoryRequirements(device, vertexBuffer, &memRequirements);
+      vkGetBufferMemoryRequirements(device, buffer, &memRequirements);
     
       VkMemoryAllocateInfo allocInfo{};
       allocInfo.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
       allocInfo.allocationSize=memRequirements.size;
-      allocInfo.memoryTypeIndex=findMemoryType(memRequirements.memoryTypeBits, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT);
+      allocInfo.memoryTypeIndex=findMemoryType(memRequirements.memoryTypeBits, properties);
     
-      if (vkAllocateMemory(device, &allocInfo, nullptr, &vertexBufferMemory) != VK_SUCCESS) {
-        throw std::runtime_error("failed to allocate vertex buffer memory");
+      if (vkAllocateMemory(device, &allocInfo, nullptr, &bufferMemory) != VK_SUCCESS) {
+        throw std::runtime_error("failed to allocate buffer memory");
       }
 
-      vkBindBufferMemory(device, vertexBuffer, vertexBufferMemory, 0);
-    
-      void* data;
-      vkMapMemory(device, vertexBufferMemory, 0, bufferInfo.size,0,&data);
-      memcpy(data,verticies.data(),(size_t) bufferInfo.size);
-      vkUnmapMemory(device, vertexBufferMemory);
+      vkBindBufferMemory(device, buffer,bufferMemory, 0);
     }
 
     uint32_t findMemoryType(uint32_t typeFilter, VkMemoryPropertyFlags properties){
