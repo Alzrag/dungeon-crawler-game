80dee24f03648093b8a75b42f146bf233619f47c
Alzrag <jediclark14@icloud.com>
2026-04-15 10:35:09 -0600

    spinny boi also on alignment requirements

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include <cstddef>
 #include <cstdint>
+#include <glm/ext/matrix_clip_space.hpp>
+#include <glm/ext/matrix_transform.hpp>
+#include <glm/trigonometric.hpp>
 #include <glm/vector_relational.hpp>
 #include <iterator>
 #include <limits>
@@ -21,6 +24,10 @@
 #include <fstream>
 #include <glm/glm.hpp>
 #include "vertex_data.hpp"
+#define GLM_FORCE_RADIANS
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <chrono>
 
 const uint32_t WIDTH = 800;
 const uint32_t HEIGHT = 600;//really dont change window size its hard
@@ -85,6 +92,14 @@ private:
     VkDeviceMemory vertexBufferMemory;
     VkBuffer indexBuffer;
     VkDeviceMemory indexBufferMemory;
+    std::vector<VkBuffer> uniformBuffers;
+    std::vector<VkDeviceMemory> uniformBuffersMemory;
+    std::vector<void*> uniformBuffersMapped;
+    VkDescriptorSetLayout descriptorSetLayout;
+    VkPipelineLayout piplineLayout;
+    VkDescriptorPool descriptorPool;
+    std::vector<VkDescriptorSet> descriptorSets;
+    
 
     static void framebufferResizeCallback(GLFWwindow* window, int width, int height){
       auto app = reinterpret_cast<HelloTriangleApplication*>(glfwGetWindowUserPointer(window));
@@ -130,6 +145,7 @@ private:
       VkDeviceSize offsets[] = {0};
       vkCmdBindVertexBuffers(commandBuffer,0,1,vertexBuffers, offsets);
       vkCmdBindIndexBuffer(commandBuffer, indexBuffer, 0, VK_INDEX_TYPE_UINT16);
+      vkCmdBindDescriptorSets(commandBuffer, VK_PIPELINE_BIND_POINT_GRAPHICS, pipelineLayout,0,1,&descriptorSets[currentFrame],0,nullptr);
       vkCmdDrawIndexed(commandBuffer, static_cast<uint32_t>(indicies.size()),1,0,0,0);
 
       vkCmdEndRenderPass(commandBuffer);
@@ -174,6 +190,12 @@ private:
       VkSurfaceCapabilitiesKHR capabilities;
       std::vector<VkSurfaceFormatKHR> formats;
       std::vector<VkPresentModeKHR> presentModes;
+    };
+
+    struct UniformBufferObject {
+      glm::mat4 model;
+      glm::mat4 view;
+      glm::mat4 proj;
     };
 
     SwarpChainSupportDetails querySwapChainSupport(VkPhysicalDevice device){
@@ -482,13 +504,103 @@ private:
       createSwapChain();
       createImageViews();
       createRenderPass();
+      createDescriptorSetLayout();
       createGraphicsPipeline();
       createFramebuffers();
       createCommandPool();
       createVertexBuffer();
       createIndexBuffer();
+      createUniformBuffers();
+      createDescriptorPool();
+      createDescriptorSets();
       createCommandBuffer();
       createSyncObjects();
+    }
+
+    void createDescriptorPool(){
+      VkDescriptorPoolSize poolSize{};
+      poolSize.type=VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
+      poolSize.descriptorCount=static_cast<uint32_t>(MAX_FRAMES_IN_FLIGHT);
+      
+      VkDescriptorPoolCreateInfo poolInfo{};
+      poolInfo.sType=VK_STRUCTURE_TYPE_DESCRIPTOR_POOL_CREATE_INFO;
+      poolInfo.poolSizeCount=1;
+      poolInfo.pPoolSizes=&poolSize;
+      poolInfo.maxSets=static_cast<uint32_t>(MAX_FRAMES_IN_FLIGHT);
+
+      if (vkCreateDescriptorPool(device, &poolInfo, nullptr,&descriptorPool)!=VK_SUCCESS){
+        throw std::runtime_error("failed to create descirptor pool");
+      }
+    }
+
+    void createDescriptorSets(){
+      std::vector<VkDescriptorSetLayout> layouts(MAX_FRAMES_IN_FLIGHT, descriptorSetLayout);
+      VkDescriptorSetAllocateInfo allocInfo{};
+      allocInfo.sType=VK_STRUCTURE_TYPE_DESCRIPTOR_SET_ALLOCATE_INFO;
+      allocInfo.descriptorPool=descriptorPool;
+      allocInfo.descriptorSetCount=static_cast<uint32_t>(MAX_FRAMES_IN_FLIGHT);
+      allocInfo.pSetLayouts=layouts.data();
+
+      descriptorSets.resize(MAX_FRAMES_IN_FLIGHT);
+      if (vkAllocateDescriptorSets(device, &allocInfo, descriptorSets.data())!=VK_SUCCESS){
+        throw std::runtime_error("failed to allocate descriptor sets");
+      }
+
+      for (size_t i =0;i<MAX_FRAMES_IN_FLIGHT;i++){
+        VkDescriptorBufferInfo bufferInfo{};
+        bufferInfo.buffer=uniformBuffers[i];
+        bufferInfo.offset=0;
+        bufferInfo.range=sizeof(UniformBufferObject);
+
+        VkWriteDescriptorSet descriptorWrite{};
+        descriptorWrite.sType=VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET;
+        descriptorWrite.dstSet=descriptorSets[i];
+        descriptorWrite.dstBinding=0;
+        descriptorWrite.dstArrayElement=0;
+        descriptorWrite.descriptorType=VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
+        descriptorWrite.descriptorCount=1;
+        descriptorWrite.pBufferInfo=&bufferInfo;
+        descriptorWrite.pImageInfo=nullptr;
+        descriptorWrite.pTexelBufferView=nullptr;
+        
+        vkUpdateDescriptorSets(device, 1, &descriptorWrite, 0, nullptr);
+      }
+    }
+
+    void createUniformBuffers(){
+      VkDeviceSize buffersSize = sizeof(UniformBufferObject);
+      uniformBuffers.resize(MAX_FRAMES_IN_FLIGHT);
+      uniformBuffersMemory.resize(MAX_FRAMES_IN_FLIGHT);
+      uniformBuffersMapped.resize(MAX_FRAMES_IN_FLIGHT);
+
+      for (size_t i = 0;i<MAX_FRAMES_IN_FLIGHT;i++){
+        createBuffer(buffersSize, VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT, uniformBuffers[i], uniformBuffersMemory[i]);
+        vkMapMemory(device, uniformBuffersMemory[i],0,buffersSize,0,&uniformBuffersMapped[i]);
+
+      }
+    }
+
+    void createDescriptorSetLayout(){
+      VkDescriptorSetLayoutBinding uboLayoutBinding{};
+      uboLayoutBinding.binding=0;
+      uboLayoutBinding.descriptorType=VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
+      uboLayoutBinding.descriptorCount=1;
+      uboLayoutBinding.stageFlags=VK_SHADER_STAGE_VERTEX_BIT;
+      uboLayoutBinding.pImmutableSamplers=nullptr;
+
+      VkDescriptorSetLayoutCreateInfo layoutInfo{};
+      layoutInfo.sType= VK_STRUCTURE_TYPE_DESCRIPTOR_SET_LAYOUT_CREATE_INFO;
+      layoutInfo.bindingCount=1;
+      layoutInfo.pBindings=&uboLayoutBinding;
+      
+      if (vkCreateDescriptorSetLayout(device, &layoutInfo, nullptr, &descriptorSetLayout) != VK_SUCCESS){
+        throw std::runtime_error("failed to create descriptor set layout");
+      }
+      
+      VkPipelineLayoutCreateInfo piplineLayoutInfo{};
+      piplineLayoutInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
+      piplineLayoutInfo.setLayoutCount=1;
+      piplineLayoutInfo.pSetLayouts=&descriptorSetLayout;
     }
 
     void createIndexBuffer(){
@@ -780,6 +892,8 @@ private:
       rasterizer.depthBiasConstantFactor=0.0f;
       rasterizer.depthBiasClamp=0.0f;
       rasterizer.depthBiasSlopeFactor=0.0f;
+      rasterizer.cullMode=VK_CULL_MODE_BACK_BIT;
+      rasterizer.frontFace=VK_FRONT_FACE_COUNTER_CLOCKWISE;
 
       //anti-aliasing
       VkPipelineMultisampleStateCreateInfo multisampling{};
@@ -816,8 +930,8 @@ private:
 
       VkPipelineLayoutCreateInfo pipelineLayoutInfo{};
       pipelineLayoutInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
-      pipelineLayoutInfo.setLayoutCount=0;
-      pipelineLayoutInfo.pSetLayouts=nullptr;
+      pipelineLayoutInfo.setLayoutCount=1;
+      pipelineLayoutInfo.pSetLayouts=&descriptorSetLayout;
       pipelineLayoutInfo.pushConstantRangeCount=0;
       pipelineLayoutInfo.pPushConstantRanges=nullptr;
       if(vkCreatePipelineLayout(device, &pipelineLayoutInfo, nullptr, &pipelineLayout)!=VK_SUCCESS){
@@ -1048,6 +1162,7 @@ private:
       VkSemaphore signalSemaphores[]={renderFinishedSemaphores[imageIndex]};
       VkSemaphore waitSemaphores[] = {imageAvailableSemaphores[currentFrame]};
       VkPipelineStageFlags waitStages[]={VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT};
+      updateUniformBuffer(currentFrame);
       VkSubmitInfo submitInfo{};
       submitInfo.sType=VK_STRUCTURE_TYPE_SUBMIT_INFO;
       submitInfo.waitSemaphoreCount=1;
@@ -1075,6 +1190,21 @@ private:
       vkQueuePresentKHR(presentQueue, &presentInfo);
       currentFrame = (currentFrame + 1) % MAX_FRAMES_IN_FLIGHT;
     } 
+
+    //spinning time look at me later for handling rotation of objects or rotation of camera more likley
+    void updateUniformBuffer(uint32_t currentImage){
+      static auto startTime = std::chrono::high_resolution_clock::now();
+      auto currentTime = std::chrono::high_resolution_clock::now();
+      float time = std::chrono::duration<float, std::chrono::seconds::period>(currentTime - startTime).count();
+
+      UniformBufferObject ubo{};
+      ubo.model=glm::rotate(glm::mat4(1.0f), time*glm::radians(90.0f), glm::vec3(0.0f,0.0f,1.0f));
+      
+      ubo.view=glm::lookAt(glm::vec3(2.0f,2.0f,2.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f,0.0f,1.0f));
+      ubo.proj=glm::perspective(glm::radians(45.0f), swapChainExtent.width/(float)swapChainExtent.height, 0.1f, 10.0f);
+      ubo.proj[1][1]*=-1;
+      memcpy(uniformBuffersMapped[currentImage], &ubo, sizeof(ubo));
+    }
 
     //this is a functiont o detroy the beguv messagner extion layer basically a destructor with weird iplimention if i understand property 
     void DestroyDebugUtilsMessengerEXT(VkInstance instance, VkDebugUtilsMessengerEXT debugMessenger, const VkAllocationCallbacks* pAllocator){
@@ -1121,6 +1251,13 @@ private:
       vkDestroyCommandPool(device, commandPool, nullptr);
 
       cleanupSwapChain();
+      
+      for (size_t i = 0; i < MAX_FRAMES_IN_FLIGHT; i++){
+        vkDestroyBuffer(device, uniformBuffers[i], nullptr);
+        vkFreeMemory(device, uniformBuffersMemory[i], nullptr);
+      }
+
+      vkDestroyDescriptorSetLayout(device, descriptorSetLayout,nullptr);
       vkDestroyBuffer(device, indexBuffer, nullptr);
       vkFreeMemory(device, indexBufferMemory, nullptr);
       vkDestroyBuffer(device, vertexBuffer, nullptr);
@@ -1128,6 +1265,9 @@ private:
       vkDestroyPipeline(device, graphicsPipeline, nullptr);
       vkDestroyPipelineLayout(device, pipelineLayout, nullptr);
       vkDestroyRenderPass(device, renderPass, nullptr);
+
+      vkDestroyDescriptorPool(device, descriptorPool, nullptr);
+      vkDestroyDescriptorSetLayout(device, descriptorSetLayout, nullptr);
  
       vkDestroyDevice(device, nullptr);
       if (enableValidationLayers){
