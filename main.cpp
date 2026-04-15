373cbc3aca3f5d8aa618895086ab5d072d022136
Alzrag <jediclark14@icloud.com>
2026-04-15 15:15:02 -0600

    little update on https://vulkan-tutorial.com/Texture_mapping/Images

--- main.cpp
+++ main.cpp
@@ -82,7 +82,7 @@ private:
     VkPipeline graphicsPipeline;//ferb we did it
     std::vector<VkFramebuffer> swapChainFramebuffers;
     VkCommandPool commandPool;
-  std::vector<VkCommandBuffer> commandBuffers;
+    std::vector<VkCommandBuffer> commandBuffers;
     //dancing aroudn emmeory acsess issues
     const int MAX_FRAMES_IN_FLIGHT = 2;
     int currentFrame = 0;
@@ -101,6 +101,8 @@ private:
     VkPipelineLayout piplineLayout;
     VkDescriptorPool descriptorPool;
     std::vector<VkDescriptorSet> descriptorSets;
+    VkImage textureImage;
+    VkDeviceMemory textureImageMemory;
 
     static void framebufferResizeCallback(GLFWwindow* window, int width, int height){
       auto app = reinterpret_cast<HelloTriangleApplication*>(glfwGetWindowUserPointer(window));
@@ -536,6 +538,37 @@ private:
       memcpy(data, pixels, static_cast<size_t>(imageSize));
       vkUnmapMemory(device, stagingBufferMemory);
       stbi_image_free(pixels);
+
+      VkImageCreateInfo imageInfo{};
+      imageInfo.sType=VK_STRUCTURE_TYPE_IMAGE_CREATE_INFO;
+      imageInfo.imageType=VK_IMAGE_TYPE_2D;
+      imageInfo.extent.width=static_cast<uint32_t>(texWidth);
+      imageInfo.extent.height=static_cast<uint32_t>(texheight);
+      imageInfo.extent.depth=1;
+      imageInfo.mipLevels=1;
+      imageInfo.arrayLayers=1;
+      imageInfo.format=VK_FORMAT_R8G8B8A8_SRGB;
+      imageInfo.tiling=VK_IMAGE_TILING_OPTIMAL;
+      imageInfo.initialLayout=VK_IMAGE_LAYOUT_UNDEFINED;
+      imageInfo.usage=VK_IMAGE_USAGE_TRANSFER_DST_BIT|VK_IMAGE_USAGE_SAMPLED_BIT;
+      imageInfo.sharingMode=VK_SHARING_MODE_EXCLUSIVE;
+      imageInfo.samples=VK_SAMPLE_COUNT_1_BIT;
+      imageInfo.flags=0;
+
+      if (vkCreateImage(device, &imageInfo, nullptr, &textureImage)!=VK_SUCCESS){
+        throw std::runtime_error("fialed to create image");
+      }
+
+      VkMemoryRequirements memRequirements;
+      vkGetImageMemoryRequirements(device, textureImage, &memRequirements);
+      VkMemoryAllocateInfo allocInfo{};
+      allocInfo.sType=VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
+      allocInfo.allocationSize=memRequirements.size;
+      allocInfo.memoryTypeIndex=findMemoryType(memRequirements.memoryTypeBits, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT);
+      
+      if(vkAllocateMemory(device, &allocInfo, nullptr, &textureImageMemory)!=VK_SUCCESS){
+        throw std::runtime_error("")//stoped here^
+      }
     }
 
     void createDescriptorPool(){
