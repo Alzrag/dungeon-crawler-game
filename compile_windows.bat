9d54fa4894dff5da0e1f53ec41e7d1e01cc95179
Alzrag <jediclark14@icloud.com>
2026-04-21 21:25:37 -0600

    more windows compatability

--- compile_windows.bat
+++ compile_windows.bat
@@ -1,2 +1,2 @@
-glslc.exe shaders\shader.vert -o shaders\vert.spv
-glslc.exe shaders\shader.frag -o shaders\frag.spv
+C:\VulkanSDK\1.4.341.1\Bin\glslc.exe shaders\shader.vert -o shaders\vert.spv
+C:\VulkanSDK\1.4.341.1\Bin\glslc.exe shaders\shader.frag -o shaders\frag.spv
